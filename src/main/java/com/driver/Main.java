f0b89c37dc1673201b910bfb719cd4c1cbff173b
SwarnaSkyla <swarnaskyla@gmail.com>
2023-01-12 22:43:40 +0530

    changes made

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,30 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
+    static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public int product(double x,double y){
+            return (int)(x*y);
+        }
+    }
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        double u=sc.nextDouble();
+        double v=sc.nextDouble();
         Product p=new Product();
-        p.product(1,1);
-        p.product(1,1,1);
-        p.product(1.00,1.00);
+        p.product(x,y);
+        p.product(x,y,z);
+        p.product(u,v);
     }
-}
- class Product{
-    public int product(int x,int y){
-        return x*y;
-    }
-     public int product(int x,int y,int z){
-         return x*y*z;
-     }
-     public int product(double x,double y){
-          return (int)(x*y);
-     }
 }
