f33fd7334066179796663caad6cdf71c7046feb7
SwarnaSkyla <swarnaskyla@gmail.com>
2023-01-12 21:58:54 +0530

    assign

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,21 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(1,1);
+        p.product(1,1,1);
+        p.product(1.00,1.00);
+    }
+}
+ class Product{
+    public int product(int x,int y){
+        return x*y;
+    }
+     public int product(int x,int y,int z){
+         return x*y*z;
+     }
+     public int product(double x,double y){
+          return (int)(x*y);
+     }
 }
